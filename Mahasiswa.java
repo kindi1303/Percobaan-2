50cd8c9f5068109ff5cef62d5ce5e4160a980e6e
Kindi <104336895+kindi1303@users.noreply.github.com>
2023-11-27 14:40:16 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa {
+    String Nama, Nim, Hobby;
+
+}
+class main {
+    public static void main(String[] args) {
+        Mahasiswa Mahasiswa1 = new Mahasiswa();
+        Mahasiswa1.Nama = "Kindiawati";
+        
+
+
+
+    }
+}
